9233d3d0981ca083cefb604854cdfa126ad36970
RalucaMaria <37650207+RalucaMaria@users.noreply.github.com>
2019-03-11 10:14:59 +0200

    Add files via upload

---
+++ Docs/Lab01/3-ProiectAgenda/ProiectAgenda/src/main/java/agenda/validator/ValidatorContact.java
@@ -0,0 +1,26 @@
+package agenda.validator;
+
+import agenda.model.base.Contact;
+
+import java.util.List;
+
+public class ValidatorContact implements Validator<Contact>{
+
+    public void validate(Contact entity) throws ValidatorException {
+        String errMsg="";
+        if (entity.getName().equals(""))
+            errMsg+="Contactul nu are nume";
+        if (entity.getAddress().equals(""))
+            errMsg+="Contactul nu are adresa";
+        if (entity.getTelefon().equals(""))
+            errMsg+="Contactul nu are nr de telefon";
+        if (errMsg!="")
+            throw new ValidatorException(errMsg);
+    }
+
+    @Override
+    public void validate(Contact contact, List<String> errors) {
+
+    }
+
+}
---
+++ Docs/Lab01/3-ProiectAgenda/ProiectAgenda/src/main/java/agenda/validator/ValidatorException.java
@@ -0,0 +1,11 @@
+package agenda.validator;
+
+public class ValidatorException extends Exception {
+
+    public ValidatorException(String message) {
+        super(message);
+    }
+
+
+
+}
